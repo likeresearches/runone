de6f37fd27fba8ede70fb93e7cafefcfd0b62c12
Eduardo Lacerda <eduardo@Eduardos-MacBook-Pro.local>
2015-09-29 16:53:16 -0300

    Pega dados GPS

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,44 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  #btnGPS */
+    $(document).on("click", "#btnGPS", function(evt)
+    {
+        //Resultado para quando capturar a posição GPS
+        var fnCapturar = function aoCapturar(position){
+            //Gravar dados da posição capturada em uma variável
+            var coords = position.coords;
+            //Exibir dados das coordenadas capturadas
+            //navigator.notification.alert(JSON.stringify(coords),"COORDENADAS");
+            
+            $("#txtLatitude").val(coords.latitude);
+            $("#txtLongitude").val(coords.longitude);
+            $("#txtAccuracy").val(coords.accuracy);
+            $("#txtAltitude").val(coords.altitude);
+            $("#txtHeading").val(coords.heading);
+            $("#txtSpeed").val(coords.speed);
+            $("#txtAltitudeAcc").val(coords.altitudeAccuracy);
+        }
+        
+        
+        
+        var fnFalhar = function(error){
+            navigator.notification.alert("Erro ao capturar: "+ error.message, "INFORMARÇAO");
+        }
+        
+        var opcoes = { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true};
+        
+        navigator.geolocation.getCurrentPosition(fnCapturar, fnFalhar, opcoes);
+        
+    });
+    
+ }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
